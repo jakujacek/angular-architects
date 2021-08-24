16b2e6f0923ba61c9c0e1f0e689edf9d570ad352
Jacek Jakubiak <jjk@spyro-soft.com>
2021-08-24 14:53:30 +0200

    nx change

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
+import { LoggerModule } from '@flight-workspace/logger-lib';
 import { CheckinComponent } from './checkin.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    LoggerModule,
     LuggageDomainModule,
+    LuggageUiCardModule,
     RouterModule.forChild([
       {
         path: '',
